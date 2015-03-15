09c02e95ed2092e5790290a99b120474c4fb2c33
arve0 <arve.seljebu@gmail.com>
2015-03-16 00:44:43 +0100

    empty test

--- test/test_leicaautomator.py
+++ test/test_leicaautomator.py
@@ -2,18 +2,7 @@
 Tests for `leicaautomator` module.
 """
 import pytest
-from leicaautomator import leicaautomator
+from leicaautomator import automator
 
-
-class TestLeicaautomator(object):
-
-    @classmethod
-    def setup_class(cls):
-        pass
-
-    def test_something(self):
-        pass
-
-    @classmethod
-    def teardown_class(cls):
-        pass
+def test_something():
+    pass
