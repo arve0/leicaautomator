8f56855abce1060b41e771093aa04a2c598bae27
arve0 <arve.seljebu@gmail.com>
2015-03-27 14:55:40 +0100

    calc average well distance and well offsets

--- leicaautomator/automator.py
+++ leicaautomator/automator.py
@@ -35,6 +35,7 @@ from skimage import io, transform
 from .viewer import *
 from leicascanningtemplate import ScanningTemplate
 from os import path
+import numpy as np
 
 def find_spots(experiment):
     """Finds spots and return position of them in a list.
@@ -126,4 +127,72 @@ def find_spots(experiment):
         region.real_x = real_x_start + region.x*x_px_size
         region.real_y = real_y_start + region.y*y_px_size
 
+    x_wells = sorted(set(r.well_x for r in regions))
+    y_wells = sorted(set(r.well_y for r in regions))
+
+    # find mean dx/dy between wells
+    dxs = []
+    prev_median = get_x_median(regions, x_wells[0])
+    for x in x_wells[1:]:
+        median = get_x_median(regions, x)
+        dxs.append(median - prev_median)
+        prev_median = median
+
+    dys = []
+    prev_median = get_y_median(regions, y_wells[0])
+    for y in y_wells[1:]:
+        median = get_y_median(regions, y)
+        dys.append(median - prev_median)
+        prev_median = median
+
+    dx = np.mean(dxs)
+    dy = np.mean(dys)
+    print('Well distance x in microns:', str(dx*1e6))
+    print('Well distance y in microns:', str(dy*1e6))
+
+    # print offsets
+    first_x, first_y = next(((r.real_x, r.real_y) for r in regions
+                            if r.well_x == 0 and r.well_y == 0), None)
+    print('============')
+    print('Well offsets')
+    print('============')
+    print('( well )   x[um], y[um]')
+    print('-----------------------')
+
+    for r in regions:
+        expected_x, expected_y = (first_x + dx*r.well_x, first_y + dy*r.well_y)
+        offset_x = round((r.real_x - expected_x)*1e6)
+        offset_y = round((r.real_y - expected_y)*1e6)
+        # only print if offset above 1 micron
+        if abs(offset_x) > 10 or abs(offset_y) > 1:
+            print('(%2d, %2d)   %5d, %5d' % (r.well_x, r.well_y, offset_x, offset_y))
+
+
+
     return labels, regions
+
+
+def get_x_median(regions, x):
+    """Get median position of given regions.
+
+    Parameters
+    ----------
+    regions : list of RegionProperties
+        List of regions with region.real_x and region.real_y set.
+    x : int
+        Which x well position to get median for.
+    """
+    return np.median([r.real_x for r in regions if r.well_x == x])
+
+
+def get_y_median(regions, y):
+    """Get median position of given regions.
+
+    Parameters
+    ----------
+    regions : list of RegionProperties
+        List of regions with region.real_x and region.real_y set.
+    y : int
+        Which y well position to get median for.
+    """
+    return np.median([r.real_y for r in regions if r.well_y == y])
