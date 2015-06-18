911741a47c903a16c59e5343c60bcea4597aa1ac
arve0 <arve.seljebu@gmail.com>
2015-06-18 19:19:35 +0200

    add test

--- test/test_leicaautomator.py
+++ test/test_leicaautomator.py
@@ -2,7 +2,23 @@
 Tests for `leicaautomator` module.
 """
 import pytest
-from leicaautomator import automator
+from leicaautomator.automator import find_tma_regions
+from leicaautomator.utils import stitch
+from py import path
 
-def test_something():
-    pass
+@pytest.fixture
+def experiment(tmpdir):
+    "'experiment--test' in tmpdir. Returns Experiment object."
+    from leicaexperiment import Experiment
+    e = path.local(__file__).dirpath().join('experiment')
+    e.copy(tmpdir.mkdir('experiment'))
+
+    return Experiment(tmpdir.join('experiment').strpath)
+
+
+def test_stitch(experiment):
+    stitched, offset = stitch(experiment)
+
+    assert offset[0] < 0, "offset between rows should be negative"
+    assert offset[1] < 0, "offset between cols should be negative"
+
